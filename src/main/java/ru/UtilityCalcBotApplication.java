ef9f1afc7cd14bb5e0d9a6672930c8f554952339
Сергей Сыров <sergejsyrov@MacSer.local>
2026-01-28 13:44:27 +0300

    исправление инициализации бота

--- src/main/java/ru/UtilityCalcBotApplication.java
+++ src/main/java/ru/UtilityCalcBotApplication.java
@@ -1,14 +1,27 @@
 package ru;
+
 import org.telegram.telegrambots.meta.TelegramBotsApi;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 import org.telegram.telegrambots.updatesreceivers.DefaultBotSession;
+import ru.UtilityCalcPk.tariff.TariffRepository;
+import ru.UtilityCalcPk.tariff.TariffService;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class UtilityCalcBotApplication {
     public static void main(String[] args) {
         try {
-            var bot = new UtilityCalc();
+            // 1. Репозиторий тарифов
+            TariffRepository repo = new TariffRepository();
+
+            // 2. (позже) загрузка тарифов в repo
+            // MosRuTariffLoader.load(repo);  // здесь будет твой код загрузки
+
+            // 3. Сервис тарифов
+            TariffService tariffService = new TariffService(repo);
+
+            // 4. Бот с сервисом
+            var bot = new UtilityCalc(tariffService);
+
+            // 5. Регистрация бота
             var botsApi = new TelegramBotsApi(DefaultBotSession.class);
             botsApi.registerBot(bot);
             System.out.println("UtilityCalcBot запущен!");
